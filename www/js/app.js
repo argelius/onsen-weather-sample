4c4981218260c9bddca67762cc8f0e9d3eaba7b5
Andreas Argelius <andreas@asial.co.jp>
2014-12-15 01:50:53 +0900

    Added menu to add and remove cities.

--- www/js/app.js
+++ www/js/app.js
@@ -3,19 +3,102 @@
 
   angular.module('app', ['onsen'])
 
-  .controller('WeatherController', function($scope, $geolocation) {
-    $scope.getWeather = function() {
+  .controller('WeatherController', function($scope, $window, $geolocation, $weather, $interval) {
+    ons.ready(function() {
+      ons.createPopover('menu.html').then(
+        function(popover) {
+          $scope.menu = popover;
+        }
+      );
+    });
 
-    };
+    $scope.places = angular.fromJson($window.localStorage.getItem('places') || '[]');
 
     $scope.updateLocalWeather = function() {
       $geolocation.get().then(
         function(position) {
+          $weather.byLocation(position.coords).then(
+            function(weather) {
+              console.log(weather);
+              $scope.localWeather = weather;
+              setImmediate(function() {
+                app.carousel.refresh();
+              });
+            }
+          );
         }
       );
     };
-
     $scope.updateLocalWeather();
+
+    $scope.updateWeather = function() {
+      $scope.updateLocalWeather();
+
+      for (var i = 0, l = $scope.places.length; i < l; i ++) {
+        var place = $scope.places[i];
+
+        (function(i) {
+          $weather.byCityId(place.id).then(
+            function(result) {
+              $scope.places[i] = result;
+            }
+          );
+        })(i);
+      }
+    };
+
+    // Update weather every minute.
+    $interval($scope.updateWeather, 60000);
+  })
+
+  .controller('MenuController', function($scope, $weather, $window) {
+    $scope.search = function() {
+      app.searchButton.startSpin();
+
+      $weather.byCityName($scope.query).then(
+        function(result) {
+          for (var i = 0, l = $scope.places.length; i < l; i ++) {
+            var place = $scope.places[i];
+            if (place.id === result.id) {
+              ons.notification.alert({
+                message: 'City already added.'
+              });
+              return;
+            }
+          }
+
+          $scope.places.push(result);
+          $window.localStorage.setItem('places', angular.toJson($scope.places));
+
+          $scope.menu.hide();
+
+          setImmediate(function() {
+            app.carousel.refresh();
+            app.carousel.setActiveCarouselItemIndex($scope.places.length + 1);
+          });
+        },
+        function() {
+          ons.notification.alert({
+            message: 'Unable to find city.'
+          });
+        }
+      )
+      .finally(
+        function() {
+          app.searchButton.stopSpin();
+        }
+      );
+    };
+    
+    $scope.removePlace = function(index) {
+      $scope.places.splice(index, 1);
+
+      $window.localStorage.setItem('places', angular.toJson($scope.places));
+
+      setImmediate(function() {
+        app.carousel.refresh();
+      });
+    };
   })
 
   .factory('$geolocation', function($q) {
@@ -53,12 +136,13 @@
   })
 
   .factory('$weather', function($q, $http) {
-    this.search = function(query) {
+    var API_ROOT = 'http://api.openweathermap.org/data/2.5/';
+
+    this.byCityName = function(query) {
       var deferred = $q.defer();
   
-      $http.jsonp('http://api.openweathermap.org/data/2.5/weather?callback=JSON_CALLBACK&q=' + encodeURI(query)).then(
+      $http.jsonp(API_ROOT + '/weather?callback=JSON_CALLBACK&q=' + encodeURI(query)).then(
         function(response) {
-          // Buggy API. Status code is sometimes a string and sometimes an integer.
           var statusCode = parseInt(response.data.cod);
 
           if (statusCode === 200) {
@@ -69,12 +153,56 @@
           }
         },
         function(error) {
-          console.log(error);
+          deferred.reject(error);
         }
       );  
 
       return deferred.promise;
     }; 
+
+    this.byCityId = function(id) {
+      var deferred = $q.defer();
+
+      $http.jsonp(API_ROOT + '/weather?callback=JSON_CALLBACK&id=' + id).then(
+        function(response) {
+          var statusCode = parseInt(response.data.cod);
+
+          if (statusCode === 200) {
+            deferred.resolve(response.data);
+          }
+          else {
+            deferred.reject(response.data.message);
+          }
+        },
+        function(error) {
+          deferred.reject(error); 
+        }
+      );
+
+      return deferred.promise;
+    };
+
+    this.byLocation = function(coords) {
+      var deferred = $q.defer();
+
+      $http.jsonp(API_ROOT + '/weather?callback=JSON_CALLBACK&lat=' + coords.latitude + '&lon=' + coords.longitude).then(
+        function(response) {
+          var statusCode = parseInt(response.data.cod);
+
+          if (statusCode === 200) {
+            deferred.resolve(response.data);
+          }
+          else {
+            deferred.reject(response.data.message);
+          }
+        },
+        function(error) {
+          deferred.reject(error);
+        }
+      );
+        
+      return deferred.promise;
+    };
 
     return this;
   })
